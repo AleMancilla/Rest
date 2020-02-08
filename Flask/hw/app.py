e343e6881eade91f1e92d3ce8918c6637f3576a5
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-08 14:34:19 -0400

    ruta para id

--- Flask/hw/app.py
+++ Flask/hw/app.py
@@ -26,7 +26,13 @@ def get_framework_by_name(name):
     return jsonify(framework[0])
 
 
-
+@app.route("/api/frameworks/<int:id>")
+def get_framework_by_id(id):
+    framework = []
+    for f in frameworks:
+        if(f["id"] == id):
+            framework.append(f)
+    return framework[0]
 
 
 if __name__ == "__main__":
