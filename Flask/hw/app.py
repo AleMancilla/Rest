ec8b644c6438f15f88b359f203d8feb0c7bfdbd6
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-08 14:03:06 -0400

    primer programa con GET json

--- Flask/hw/app.py
+++ Flask/hw/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+frameworks = [
+    { "id":1,  "name":"flask" },
+    { "id":2,  "name":"ExpressJS" },
+    { "id":3,  "name":"Laravel" }
+]
 @app.route("/")
 def index():
     return "Hello World!"
 
+@app.route("/api/frameworks/",methods=["GET"]) # creamos una ruta para nuestra aplicacion 
+#el metodo get son las rutas de flask se crean por defecto...  nosotros lo añadimos para ser mas especificos
+def get_frameworks():
+
+    return jsonify(frameworks)
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True) #en flask podemos activar el modo debug
+    #el cual nos ayuda editar los archivos que pertenecen a nuestra aplicacion
+    #guardarlos y que el servidor detecte esos cambios y se reinicie
