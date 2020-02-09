609d12e3673dd843945f4855eabe981fdf2a2b6e
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-02-09 00:53:38 -0400

    creamos el modelo de la base de datos a partir de python en el entorno virtual siguiendo pasos de https://www.youtube.com/watch?v=tFioTCexYGc&list=PLBO4apWPK7b6tRI_B1MQGZfvI6c3ViQd-&index=11

--- Flask/data_persistence/sqldbs/finished/app.py
+++ Flask/data_persistence/sqldbs/finished/app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+#necesitamos un conector y una ruta absoluta para la base de datos#
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))#ruta absoluta
+DB_URI = "sqlite:///" + os.path.join(BASE_DIR, "database.db") #
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI #este no debe faltar
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] =False
+db = SQLAlchemy(app) 
+
+class Framework(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    #name = db.Colum(db.String(50), nullable = False , unique = True)
+    name = db.Column(db.String(50))
+    
 
 @app.route("/")
 def index():
     return "Hello World! dbs"
-
-if __name__ == "__main__":
-    app.run()
 
